52c00b2c9db4c3a65319e379626fbb27f2278b70
LeleDerGrasshalmi <king-of-gosts@web.de>
2024-11-16 14:17:02 +0100

    blacklist a lot of eulas

--- src/resources/eulas.ts
+++ src/resources/eulas.ts
@@ -1,37 +1,8 @@
 export const eulas: string[] = [
   // affiliate
   'sac_agreement',
-  'affiliate',
   'affiliate_agreement',
   'ic_agreement',
-
-  // store
-  'egstore',
-  'android-egs',
-  'android-portal',
-
-  // privacy
-  'epic_content',
-  'epic-guardian-terms',
-  'epic-personal-info-collection',
-  'epic-personal-info-transfer',
-  'epic-promo-notification',
-  'epic-rewards-terms',
-  'epic-tou',
-  'epicgames-tos',
-  'egauthenticator',
-  'crab', // epic test eula
-  'min', // epic test eula
-
-  // eos
-  'eos',
-  'eos_account_services',
-  'eos_anticheat_services',
-  'eos_developer_agreement',
-  'eos_epic_achievement_developer_agreement',
-  'eos_standard_services',
-  'egs_spt_customer_email_sharing_agreement',
-  'egs_spt_distribution_agreement',
 
   // fortnite
   'uefn',
@@ -43,29 +14,19 @@ export const eulas: string[] = [
   'fn-ps4-CUSA07669_00',
   'fn-xb1-033A70A1',
   'fn-xb1-267695549',
-  'fn_jp_payment_services_act',
-  'fn_jp_transaction_rules',
+  'mfnlaunch',
   'FortniteMobileAndroid',
   'FortniteMobileAndroid-Samsung',
   'FortniteMobileIOS',
 
-  // ue
-  'ue',
-  'ue_games',
-  'ue_non_games',
-  'ue_studio',
-  'ue_studio_custom',
+  // other
+  'egstore',
   'unreal_engine',
+  'unreal_engine2',
 
-  // subsidiaries / marketplaces
-  'marketplace_distribution',
   'fab_eula',
   'fab_marketplace',
-  'ucs-realityscan',
   'twin',
-  'twin_presenter',
-  'publishing_agreement',
-  'publishing-seller-agreement',
 ];
 
 export const locales: string[] = [
